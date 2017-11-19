1218026f056627c4c6d52f88850c5674dc4053ba
whaenel <vonhenninghausen@web.de>
2017-11-19 14:18:35 +0100

    moved parm to ext file

---
+++ extparms.groovy
@@ -0,0 +1,4 @@
+
+def extFile="comes form extern"
+
+return this;
--- jenkins.groovy
+++ jenkins.groovy
@@ -3,6 +3,7 @@
 def extFile="comes form extern"
 node {
    def mvnHome
+   Parms=load "extparms.groovy"
    stage('Preparation') { // for display purposes
       // Get some code from a GitHub repository
       git 'https://github.com/whaenel/JenkinsTest.git'
@@ -10,9 +11,9 @@ node {
    }
    stage('Build') {
       // Run the maven build
-      print extFile
+      print Parms.extFile
    }
    stage('Results') {
-        print sucessful
+        print "sucessful"
    }
 }
