b9879904bf61fec850a180f70d212326481dbfc7
whaenel <vonhenninghausen@web.de>
2017-11-19 16:09:53 +0100

    pull from git

--- extparms.groovy
+++ extparms.groovy
@@ -1,4 +1,10 @@
 
 def extFile="comes form extern"
 
+// Methods in this file will end up as object methods on the object that load returns.
+def lookAtThis(String whoAreYou) {
+    echo "Look at this, ${whoAreYou}! You loaded this from another file!"
+}
+
+
 return this;
--- jenkins.groovy
+++ jenkins.groovy
@@ -8,8 +8,13 @@ node {
    stage('Preparation') { // for display purposes
       // Get some code from a GitHub repository
         git changelog: false, credentialsId: '7ca6d9cb-99fc-4770-886c-18f2947b67ec', url: 'https://github.com/whaenel/JenkinsTest.git'
-        Parms=load "extparms.groovy"
- 
+        def Parms=load( "extparms.groovy")
+        //def externalMethod = load("externalMethod.groovy")
+
+    // Call the method we defined in externalMethod.
+    Parms.lookAtThis("Steve")
+    print Parms.extFile
+   
       
    }
    stage('Build') {
