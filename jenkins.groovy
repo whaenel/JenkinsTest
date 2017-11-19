eac24b893f3780dd73fffeccfaf36d72468018ca
whaenel <vonhenninghausen@web.de>
2017-11-19 14:53:31 +0100

    pull from git

--- jenkins.groovy
+++ jenkins.groovy
@@ -3,6 +3,7 @@
 def extFile="comes form extern"
 node {
    def mvnHome
+   git changelog: false, credentialsId: '7ca6d9cb-99fc-4770-886c-18f2947b67ec', url: 'https://github.com/whaenel/JenkinsTest.git'
    Parms=load "extparms.groovy"
    stage('Preparation') { // for display purposes
       // Get some code from a GitHub repository
