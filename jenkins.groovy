977ea59a1db648fbb412b36608033c11b3cfe1e1
whaenel <vonhenninghausen@web.de>
2017-12-02 00:19:22 +0100

    added import statement

--- jenkins.groovy
+++ jenkins.groovy
@@ -5,10 +5,12 @@ node {
    def mvnHome
    git changelog: false, credentialsId: '7ca6d9cb-99fc-4770-886c-18f2947b67ec', url: 'https://github.com/whaenel/JenkinsTest.git'
    def Parms=load ("extparms.groovy")
+   /*
    def jHelper = load ("src/com/ibm/swf/udeploy/JenkinsHelper.groovy")
    stage('Preparation') { // for display purposes
         Parms.lookAtThis("Steve")
         print env.extFile
+        */
    }
    stage('Build') {
       // Run the maven build
